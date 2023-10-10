1cf394b27c2a3dc37c76d508c234e3e7571c7704
GabuGabriel <gabriel.munoz.solis@gmail.com>
2023-10-10 20:51:06 -0300

    tercer

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,9 @@
 const Home = () => {
-    return(
+    return (
         <>
-        <h1>Bienvenido a nuesta tienda</h1>
-        <h2>Encuentra todo lo que necesitas</h2>
-        <img src="" alt="" />
+            <h1>Bienvenido a nuesta tienda</h1>
+            <h2>Encuentra todo lo que necesitas</h2>
+            <img src="\src\assets\imgaentienda.svg" alt="" height="250px" width="150px"/>
         </>
     )
 }
