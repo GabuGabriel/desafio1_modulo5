6d114d648baca92f214458bd419ca75ed8582140
GabuGabriel <gabriel.munoz.solis@gmail.com>
2023-10-10 21:30:26 -0300

    final

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,19 @@
 import { Link } from "react-router-dom"
 
+
 const Navbar = () => {
     return (
-        <nav class="navbar bg-primary ">
-            <div class="d-flex justify-content-center ">
-                <Link  to="/"><a class="navbar-brand" href="#" >Home</a></Link>
-                <Link to="Contactos"><a class="navbar-brand" href="#">Contactos</a></Link>
+        <nav class="navbar bg-primary">
+            <div class="d-flex justify-content-center align-items-center align-content-cente">
+                <Link to="/">
+                    <img src="\src\assets\home.svg" alt="" height="20px" />
+                    <a class="navbar-brand" href="#" >Home</a>
+                </Link>
+
+                <Link to="Contactos">
+                <img src="\src\assets\contact.svg" alt="" height="20px" />
+                    <a class="navbar-brand" href="#">Contactos</a>
+                </Link>
             </div>
         </nav>
     )
