4dbc56ef72958c6b25130366d5f17d8e6ed30359
GabuGabriel <gabriel.munoz.solis@gmail.com>
2023-10-10 21:01:59 -0300

    otromas

--- src/components/Contactos.jsx
+++ src/components/Contactos.jsx
@@ -1,16 +1,16 @@
 const Contactos = () => {
     return (
         <>
-            <form>
+            <form className="formulario">
                 <fieldset disabled>
                     <legend>¿Quieres contactarte con nosotros?</legend>
                     <div class="mb-3">
-                        <label for="disabledTextInput" class="form-label">Ingresa tu Nombre</label>
-                        <input type="text" id="disabledTextInput" class="form-control" placeholder="Disabled input" />
+                        <label for="TextInput" class="form-label">Ingresa tu Nombre</label>
+                        <input type="text" id="TextInput" class="form-control" placeholder="Ingresa tu nombre" />
                     </div>
                     <div class="mb-3">
-                        <label for="disabledTextInput" class="form-label">Ingresa tu correo</label>
-                        <input type="text" id="disabledTextInput" class="form-control" placeholder="Disabled input" />
+                        <label for="TextInput" class="form-label">Ingresa tu correo</label>
+                        <input type="text" id="TextInput" class="form-control" placeholder="Ingresa tu correo" />
                     </div>
                     <button type="submit" class="btn btn-primary">Enviar</button>
                 </fieldset>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,9 @@ import { Link } from "react-router-dom"
 
 const Navbar = () => {
     return (
-        <nav class="navbar bg-primary">
-            <div class="container-fluid">
-                <Link to="/"><a class="navbar-brand" href="#">Home</a></Link>
+        <nav class="navbar bg-primary ">
+            <div class="d-flex justify-content-center ">
+                <Link  to="/"><a class="navbar-brand" href="#" >Home</a></Link>
                 <Link to="Contactos"><a class="navbar-brand" href="#">Contactos</a></Link>
             </div>
         </nav>
