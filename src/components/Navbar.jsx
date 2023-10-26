c16fd2b1d01eb15151bc232ed54cf3fc9af56001
GabuGabriel <gabriel.munoz.solis@gmail.com>
2023-10-25 21:16:11 -0300

    arreglos

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,18 @@
 import { Link } from "react-router-dom"
 
-
 const Navbar = () => {
     return (
-        <nav class="navbar bg-primary">
+        <nav class="navbar bg-primary p-4" >
             <div class="d-flex justify-content-center align-items-center align-content-cente">
-                <Link to="/">
+
+                <Link to="/" className="estilosLinks">
                     <img src="\src\assets\home.svg" alt="" height="20px" />
                     <a class="navbar-brand" href="#" >Home</a>
                 </Link>
 
-                <Link to="Contactos">
-                <img src="\src\assets\contact.svg" alt="" height="20px" />
-                    <a class="navbar-brand" href="#">Contactos</a>
+                <Link to="Contactos" className="estilosLinks">
+                    <img src="\src\assets\contact.svg" alt="" height="20px" />
+                    <a class="navbar-brand" href="#">Contacto</a>
                 </Link>
             </div>
         </nav>
